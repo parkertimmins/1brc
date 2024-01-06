99decd8548155de3c810226e955d696ebf1ba039
parkertimmins <parkertimmins@gmail.com>
2024-01-06 16:52:59 -0600

    Adding multithreading

--- src/main/java/dev/morling/onebrc/CalculateAverage_ptimmins.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_ptimmins.java
@@ -15,22 +15,15 @@
  */
 package dev.morling.onebrc;
 
-import static java.util.stream.Collectors.*;
-
 import java.nio.charset.StandardCharsets;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.stream.Collector;
 
 public class CalculateAverage_ptimmins {
     private static final String FILE = "./measurements.txt";
@@ -57,15 +50,14 @@ public class CalculateAverage_ptimmins {
         private double max = Double.NEGATIVE_INFINITY;
         private double sum;
         private long count;
-    }
 
-    // static long endOfLineAfter(MappedByteBuffer mbb, long start, long limit) {
-    // long curr = start;
-    // while (curr < limit && mbb.getChar(curr) != '\n') {
-    //
-    // }
-    // System.out.print((char) mbb.get());
-    // }
+        void merge(MeasurementAggregator other) {
+            min = Math.min(min, other.min);
+            max = Math.max(max, other.max);
+            sum += other.sum;
+            count += other.count;
+        }
+    }
 
     static class MappedRange {
         MappedByteBuffer mbb;
@@ -102,12 +94,14 @@ public class CalculateAverage_ptimmins {
         }
     }
 
-    static final int targetSize = 1 << 30; // 1GB
-    static final long maxSize = Integer.MAX_VALUE; // 2GB (needs to fit in int
+    static final long MAX_MMAP_SIZE = Integer.MAX_VALUE;
     static final int padding = 200; // max entry size is 107ish == 100 (station) + 1 (semicolon) + 5 (temp, eg -99.9) + 1 (newline)
 
-    static ArrayList<MappedRange> openMappedFiles(FileChannel channel) throws IOException {
+    static ArrayList<MappedRange> openMappedFiles(FileChannel channel, int numSplits) throws IOException {
         final long fileSize = channel.size();
+
+        final long avgSize = fileSize / numSplits;
+        final long maxSize = (long) (1.5 * avgSize);
 
         ArrayList<MappedRange> ranges = new ArrayList<>();
         long offset = 0;
@@ -116,15 +110,15 @@ public class CalculateAverage_ptimmins {
             boolean backPad;
             if (offset + maxSize >= fileSize) {
                 size = Math.toIntExact(fileSize - offset);
-                // System.out.println(STR."final mbb, size: \{size}");
                 backPad = false;
             }
             else {
-                size = targetSize;
+                size = Math.toIntExact(avgSize);
                 backPad = true;
             }
+
+            System.out.println("making split with size: " + size);
             boolean frontPad = !ranges.isEmpty();
-            System.out.println(offset + " " + size + " " + (offset + size) + " " + fileSize);
             MappedByteBuffer mbb = channel.map(FileChannel.MapMode.READ_ONLY, offset, size);
             ranges.add(new MappedRange(mbb, frontPad, backPad, size, offset));
 
@@ -153,89 +147,126 @@ public class CalculateAverage_ptimmins {
         return curr;
     }
 
-    static int findNext(MappedByteBuffer mbb, int offset, byte val) {
-        int curr = offset;
-        while (mbb.get(curr) != val) {
-            curr++;
+    static void processMappedRange(MappedRange range, final HashMap<String, MeasurementAggregator> localAgg) {
+        byte[] buf = new byte[200];
+
+        int curr = range.frontPad ? findNextEntryStart(range.mbb, 0) : 0;
+        int limit = range.backPad ? findNextEntryStart(range.mbb, range.len - padding) : range.len;
+
+        while (curr < limit) {
+            int next = findNextEntryStart(range.mbb, curr);
+
+            int endStr = findNextSemicolon(range.mbb, curr);
+
+            int stationLen = endStr - curr;
+            for (int i = 0; i < stationLen; ++i) {
+                buf[i] = range.mbb.get(curr);
+                curr++;
+            }
+
+            String station = new String(buf, 0, stationLen, StandardCharsets.UTF_8); // for UTF-8 encoding
+
+            curr = endStr + 1;
+
+            short sign = 1;
+            if (range.mbb.get(curr) == '-') {
+                sign = -1;
+                curr++;
+            }
+
+            int numDigits = next - curr - 2; // subtract one for decimal and one for end newline
+            short temp = 0;
+            if (numDigits == 3) {
+                temp += ((char) range.mbb.get(curr)) - '0';
+                temp *= 10;
+                temp += ((char) range.mbb.get(curr + 1)) - '0';
+                temp *= 10;
+                temp += ((char) range.mbb.get(curr + 3)) - '0'; // skip decimal
+            }
+            else {
+                temp += ((char) range.mbb.get(curr)) - '0';
+                temp *= 10;
+                temp += ((char) range.mbb.get(curr + 2)) - '0'; // skip decimal
+            }
+            temp *= sign;
+
+            curr = next;
+
+            double tempWithDecimal = ((double) temp) / 10;
+            var m = new Measurement(station, tempWithDecimal);
+            if (localAgg.containsKey(m.station())) {
+                var ma = localAgg.get(m.station());
+                ma.count += 1;
+                ma.sum += m.value;
+                ma.min = Math.min(m.value, ma.min);
+                ma.max = Math.max(m.value, ma.max);
+            }
+            else {
+                var ma = new MeasurementAggregator();
+                ma.count = 1;
+                ma.sum = ma.max = ma.min = m.value;
+                localAgg.put(m.station(), ma);
+            }
         }
-        return curr;
     }
 
-    public static void main(String[] args) throws IOException {
+    static HashMap<String, MeasurementAggregator> mergeAggregations(ArrayList<HashMap<String, MeasurementAggregator>> localAggs) {
+        HashMap<String, MeasurementAggregator> global = new HashMap<>();
+
+        for (var agg : localAggs) {
+            for (Map.Entry<String, MeasurementAggregator> entry : agg.entrySet()) {
+                var station = entry.getKey();
+                var ma = entry.getValue();
+
+                var currentVal = global.get(station);
+                if (currentVal != null) {
+                    currentVal.merge(ma);
+                }
+                else {
+                    global.put(station, ma);
+                }
+            }
+        }
+        return global;
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         RandomAccessFile file = new RandomAccessFile(FILE, "r");
         FileChannel channel = file.getChannel();
 
-        ArrayList<MappedRange> mappedRanges = openMappedFiles(channel);
+        int numThreads = Runtime.getRuntime().availableProcessors();
+        System.out.println("Running on " + numThreads + " threads");
 
-        byte[] buf = new byte[200];
-        final HashMap<String, MeasurementAggregator> cache = new HashMap<>();
+        long fileSize = channel.size();
+        int rangesPerThread = 3;
+        int numSplits = rangesPerThread * numThreads;
 
-        for (MappedRange range : mappedRanges) {
+        final ArrayList<MappedRange> mappedRanges = openMappedFiles(channel, numSplits);
+        final ArrayList<HashMap<String, MeasurementAggregator>> localAggs = new ArrayList<>(numThreads);
 
-            int curr = range.frontPad ? findNextEntryStart(range.mbb, 0) : 0;
-            int limit = range.backPad ? findNextEntryStart(range.mbb, range.len - padding) : range.len;
-
-            while (curr < limit) {
-                int next = findNextEntryStart(range.mbb, curr);
-
-                int endStr = findNextSemicolon(range.mbb, curr);
-
-                int stationLen = endStr - curr;
-                for (int i = 0; i < stationLen; ++i) {
-                    buf[i] = range.mbb.get(curr);
-                    curr++;
+        Thread[] threads = new Thread[numThreads];
+        for (int t = 0; t < numThreads; t++) {
+            localAggs.add(new HashMap<>());
+            int threadId = t;
+            threads[t] = new Thread(() -> {
+                var localAgg = localAggs.get(threadId);
+                for (int split = 0; split < rangesPerThread; ++split) {
+                    int splitId = rangesPerThread * threadId + split;
+                    processMappedRange(mappedRanges.get(splitId), localAgg);
                 }
+            }, "Thread-" + t);
 
-                String station = new String(buf, 0, stationLen, StandardCharsets.UTF_8); // for UTF-8 encoding
-
-                curr = endStr + 1;
-
-                short sign = 1;
-                if (range.mbb.get(curr) == '-') {
-                    sign = -1;
-                    curr++;
-                }
-
-                int numDigits = next - curr - 2; // subtract one for decimal and one for end newline
-                short temp = 0;
-                if (numDigits == 3) {
-                    temp += ((char) range.mbb.get(curr)) - '0';
-                    temp *= 10;
-                    temp += ((char) range.mbb.get(curr + 1)) - '0';
-                    temp *= 10;
-                    temp += ((char) range.mbb.get(curr + 3)) - '0'; // skip decimal
-                }
-                else {
-                    temp += ((char) range.mbb.get(curr)) - '0';
-                    temp *= 10;
-                    temp += ((char) range.mbb.get(curr + 2)) - '0'; // skip decimal
-                }
-                temp *= sign;
-
-                curr = next;
-
-                double tempWithDecimal = ((double) temp) / 10;
-                var m = new Measurement(station, tempWithDecimal);
-
-                if (cache.containsKey(m.station())) {
-                    var ma = cache.get(m.station());
-                    ma.count += 1;
-                    ma.sum += m.value;
-                    ma.min = Math.min(m.value, ma.min);
-                    ma.max = Math.max(m.value, ma.max);
-                }
-                else {
-                    var ma = new MeasurementAggregator();
-                    ma.count = 1;
-                    ma.sum = ma.max = ma.min = m.value;
-                    cache.put(m.station(), ma);
-                }
-            }
+            threads[t].start();
         }
 
+        for (var thread : threads) {
+            thread.join();
+        }
+
+        var globalAggs = mergeAggregations(localAggs);
         Map<String, ResultRow> res = new TreeMap<>();
-        for (Map.Entry<String, MeasurementAggregator> entry : cache.entrySet()) {
+        for (Map.Entry<String, MeasurementAggregator> entry : globalAggs.entrySet()) {
             var ma = entry.getValue();
             res.put(entry.getKey(), new ResultRow(ma.min, ma.sum / ma.count, ma.max));
         }
